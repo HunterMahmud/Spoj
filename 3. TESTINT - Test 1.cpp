db979e037732edcd6bf9fe011e24d18c5cc69fcf
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-20 22:25:22 +0600

    Create 3. TESTINT - Test 1.cpp

---
+++ 3. TESTINT - Test 1.cpp
@@ -0,0 +1,13 @@
+// url = https://www.spoj.com/problems/TESTINT/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a;
+	cin>>b;
+	cout<<a+b<<endl;
+	return 0;
+}

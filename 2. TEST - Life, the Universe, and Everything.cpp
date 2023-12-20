c59b04e4261ab7d7f62aac217302f71c55190863
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-20 22:05:48 +0600

    Create 2. TEST - Life, the Universe, and Everything.cpp

---
+++ 2. TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,16 @@
+// url = https://www.spoj.com/problems/TEST/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	while(true){
+		cin>>t;
+		if(t==42) break;
+		else cout<<t<<endl;
+	}
+	return 0;
+}

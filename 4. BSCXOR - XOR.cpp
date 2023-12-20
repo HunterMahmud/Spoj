f95b69c2570493ef7642507bb119b43b6e2f0685
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-20 22:29:55 +0600

    Create 4. BSCXOR - XOR.cpp

---
+++ 4. BSCXOR - XOR.cpp
@@ -0,0 +1,12 @@
+// url = https://www.spoj.com/problems/BSCXOR/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	bool x,y;
+	cin>>x>>y;
+	cout<<(x^y)<<endl;
+	return 0;
+}
